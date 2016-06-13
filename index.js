9fe1b1eb8ea5909ca57bf5a95b5986ac9fca9801
Jan Nicklas <j.nicklas@me.com>
2016-06-13 18:07:48 +0200

    Refactoring

--- index.js
+++ index.js
@@ -3,16 +3,37 @@ var mainApplication = new T.Application();
 
 module.exports = {
   mainApplication: mainApplication,
-  applyTerrific: function(domNode) {
+  startNode: function(domNode) {
     // get terrific component name
     var dataName = domNode.getAttribute('data-t-name');
     // link terrific component to dom node
     var mod = mainApplication.registerModule(domNode, dataName);
-    if ( mod ) {
-      // execute terrific component
-      mainApplication.start([mod]);
+    if (!mod) {
+      throw new Error('starting terrific component "' + dataName + '" failed.');
     }
-    // Return the public api
-    return ( mod && mod.api ) ? mod.api : {};
+    // execute terrific component
+    mainApplication.start([mod]);
+    // Return the public api and events
+    return {
+      api: mod.api || {},
+      events: mod._events
+    };
+  },
+  createModule: function(moduleName, moduleDefinitions) {
+    if (typeof moduleName !== 'string' || !/^[A-Z][a-z0-9\-]+[a-z0-9]/.test(moduleName)) {
+      throw new Error('Invalid module name "' + moduleName + '".');
+    }
+    if (T.Module[moduleName]) {
+      throw new Error('Module "' + moduleName + '" was already defined.');
+    }
+    T.Module[moduleName] = T.createModule(moduleDefinitions);
+    return T.Module[moduleName];
   }
 };
+
+// Inherit from terrific
+for (var attribute in T) {
+  if( T.hasOwnProperty( attribute ) && typeof attribute === 'function' ) {
+    module.exports[attribute] = module.exports[attribute] || T[attribute];
+  }
+}
